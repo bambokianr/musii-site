e429c241cf118eabe5d15622a974948b51c4ad21
Rafaella Bambokian <rafaellabambokian@outlook.com>
2020-05-18 23:40:29 -0400

    Footer - responsive screen ok

--- front-site/src/sections/Footer/index.js
+++ front-site/src/sections/Footer/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logoVertical from '../../assets/logo_vertical_roxo.svg';
-import { Container, Background, Logo, Content, Divider, Contact, ContactItem, ContactText, Copyright } from './styles';
+import { Container, Background, Logo, Content, Other, Divider, Contact, ContactItem, ContactText, Copyright } from './styles';
 import { MdEmail } from "react-icons/md";
 import { FaApple, FaGooglePlay } from "react-icons/fa";
 
@@ -10,15 +10,17 @@ function Footer() {
       <Container>
         <Logo src={logoVertical} alt="Logo Musii" /> 
         <Content>
-        <Contact>
-            <ContactItem><MdEmail color="#771292" size={20} /><ContactText href="mailto:contato@musii.app">contato@musii.app</ContactText></ContactItem>
-            <ContactItem><FaApple color="#771292" size={18} /><ContactText href="#" target="_blank">Baixar na App Store</ContactText></ContactItem>
-            <ContactItem><FaGooglePlay color="#771292" size={16} /><ContactText href="#" target="_blank">Baixar no Gogle Play</ContactText></ContactItem>
+          <Contact>
+            <ContactItem><MdEmail color="#771292" /><ContactText href="mailto:contato@musii.app">contato@musii.app</ContactText></ContactItem>
+            <ContactItem><FaApple color="#771292" /><ContactText href="#" target="_blank">Baixar na App Store</ContactText></ContactItem>
+            <ContactItem><FaGooglePlay color="#771292" /><ContactText href="#" target="_blank">Baixar no Gogle Play</ContactText></ContactItem>
           </Contact>
-          <Divider>-</Divider>
-          <a href="#">Política de privacidade</a>
-          <Divider>-</Divider>
-          <Copyright><span>©</span> 2020 Musii</Copyright>
+          <Other>
+            <Divider>-</Divider>
+            <a href="#">Política de privacidade</a>
+            <Divider>-</Divider>
+            <Copyright><span>©</span> 2020 Musii</Copyright>
+          </Other>
         </Content>
       </Container>
     </Background>
--- front-site/src/sections/Footer/styles.js
+++ front-site/src/sections/Footer/styles.js
@@ -7,7 +7,7 @@ export const Background = styled.div`
 `;
 
 export const Container = styled.div`
-  padding: 0 24px;
+  padding: 8px 24px;
   margin: 0 auto;
   width:100%;
   height: 100%;
@@ -17,7 +17,11 @@ export const Container = styled.div`
   align-items: flex-end;
 
   @media screen and (max-width: 1400px) {
-    padding: 0 72px;
+    padding: 12px 72px;
+  }
+
+  @media screen and (max-width:1060px) {
+    flex-direction: column;
   }
 `;
 
@@ -31,16 +35,25 @@ export const Content = styled.div`
     cursor: pointer;
     color: #565060;
   }
+
   a:hover {
     color: #cc78f7;
   }
 
+  @media screen and (max-width: 1060px) {
+    width: 100%;
+    justify-content: space-between;
+    align-items: flex-end;
+  }
 `;
 
 export const Logo = styled.img`
   width: 60px;
   margin: 6px 0;
-  /* margin-bottom: 20px; */
+
+  @media screen and (max-width: 1060px) {
+    width: 70px;
+  }
 `;
 
 export const Contact = styled.div`
@@ -50,6 +63,23 @@ export const Contact = styled.div`
   
   @media screen and (max-width: 1060px) {
     flex-direction: column;
+    margin-left: -4px;
+  }
+`;
+
+export const Other = styled.div`
+  display: flex;
+  align-items: center;
+
+  @media screen and (max-width: 1060px) {
+    flex-direction: column;
+    align-items: flex-end;
+
+    a {
+      font-size: 13px;
+      border-bottom: 1px solid #77129282;
+      padding-bottom: 6px;
+    }
   }
 `;
 
@@ -58,16 +88,26 @@ export const ContactItem = styled.span`
   align-items: center;
 
   &:nth-child(1) {
+    svg {
+      font-size: 19px;
+    }
     border-right: 1px solid #77129282;
     padding-right: 8px;
   }
 
   &:nth-child(2) {
+    svg {
+      font-size: 20px;
+      margin-bottom: 2px;
+    }
     border-right: 1px solid #77129282;
     padding: 0 8px;
   }
 
   &:nth-child(3) {
+    svg {
+      font-size: 14px;
+    }
     padding-left: 8px;
   }
   
@@ -76,11 +116,19 @@ export const ContactItem = styled.span`
       border-style: hidden;
       padding: 0;
     }
+
+    &:nth-child(3) > svg {
+      margin-left: 7px;
+    }
   }
 `;
 
 export const ContactText = styled.a`
   margin-left: 4px;
+  
+  @media screen and (max-width: 1060px) {
+    padding: 4px 6px;
+  }
 `;
 
 export const Copyright = styled.div`
@@ -95,8 +143,16 @@ export const Copyright = styled.div`
     margin-top: 3px;
     margin-right: 2px;
   }
+
+  @media screen and (max-width: 1060px) {
+    margin-bottom: -3px;
+  }
 `;
 
 export const Divider = styled.span`
   padding: 0 12px;
+
+  @media screen and (max-width: 1060px) {
+    display: none;
+  }
 `;
